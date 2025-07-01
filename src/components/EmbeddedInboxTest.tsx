7200b82c0d026b8426fa1d90b5751ba1c0b05206
Ahmad Mhaish <ahmad@dconfig.com>
2025-07-02 01:15:59 +0300

    Using the new version of inbox component after applying some fixes

--- src/components/EmbeddedInboxTest.tsx
+++ src/components/EmbeddedInboxTest.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { ApolloProvider, ServerError } from '@apollo/client';
 import { Inbox, ThemeProvider, dconfigClient, setErrorHandler } from '@bcrumbs.net/inbox';
+import { ConvsFilters } from '@bcrumbs.net/inbox/src/app/inbox/utils/convs';
 
 setErrorHandler(({ networkError, graphQLErrors }) => {
     console.log('networkError', networkError);
@@ -23,6 +24,7 @@ setErrorHandler(({ networkError, graphQLErrors }) => {
 
 const EmbeddedInboxTest: React.FC = () => {
     const [isStorageReady, setIsStorageReady] = useState(false);
+    const [filters, setFilters] = useState<ConvsFilters>({ nameOrPhone: '90507', ended: true });
 
     useEffect(() => {
         // Initialize the localStorage with API key, context id (workspace id), and user info
@@ -40,7 +42,15 @@ const EmbeddedInboxTest: React.FC = () => {
         <ApolloProvider client={dconfigClient}>
             <ThemeProvider initialThemeValues={{ rtl: true, toggleLang: () => { } }}>
                 <div className="test-wrapper" dir="rtl">
-                    <Inbox rtl={true} onConversationChange={(conv) => console.log(`Conversation with id ${conv.id} is selected`)} initialFilters={{ nameOrPhone: '553464' }} logo={"https://png.pngtree.com/png-clipart/20190614/original/pngtree-settings-line-black-icon-png-image_3767553.jpg"} />
+                    <Inbox
+                        rtl={true}
+                        onConversationChange={(conv) => console.log(`Conversation with id ${conv.id} is selected`)}
+                        initialFilters={filters}
+                        onFiltersChange={(filters) => {
+                            console.log(`Filters changed to ${JSON.stringify(filters)}`);
+                            setFilters(filters);
+                        }}
+                        logo={"https://png.pngtree.com/png-clipart/20190614/original/pngtree-settings-line-black-icon-png-image_3767553.jpg"} />
                 </div>
             </ThemeProvider>
         </ApolloProvider>
